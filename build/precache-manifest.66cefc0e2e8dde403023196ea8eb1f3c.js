0cb32d05053068ffa7d2e8a909cce45117975fa7
bscsjaymar21 <namoc@gmail.com>
2022-12-11 15:27:56 +0800

    initial commit

---
+++ build/precache-manifest.66cefc0e2e8dde403023196ea8eb1f3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d0def723cfab064f205",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/css/main.76f6d8c8.chunk.css"
+  },
+  {
+    "revision": "3d0def723cfab064f205",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/js/main.3d0def72.chunk.js"
+  },
+  {
+    "revision": "41eaf1e9fa0f02fc1cea",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/js/1.41eaf1e9.chunk.js"
+  },
+  {
+    "revision": "89ce2d0359a21a6bd2ec",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/js/runtime~main.89ce2d03.js"
+  },
+  {
+    "revision": "6a9167f398d32eb3b78bc32ac3696753",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/media/ME1.6a9167f3.jpg"
+  },
+  {
+    "revision": "acc114e802261a6ce3904a3f1ab9277d",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/media/logo.acc114e8.jpg"
+  },
+  {
+    "revision": "ac1839b05d92a3c9818f9063ad10098e",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/static/media/45-degree-fabric-light.ac1839b0.png"
+  },
+  {
+    "revision": "5832686ea36b49d71a5d5281085f4839",
+    "url": "/Vinnie-Fe-Mocoy/vinniefe.github.io/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Vinnie-Fe-Mocoy/vinniefe.github.io/precache-manifest.66cefc0e2e8dde403023196ea8eb1f3c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Vinnie-Fe-Mocoy/vinniefe.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
